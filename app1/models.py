b01d8a9281f737419b61a580d477aef36fb2d41f
ali.fesli@teilnehmer-bfwsb.de <ali.fesli@teilnehmer-bfwsb.de>
2023-07-25 15:32:57 +0200

    Model Block angelegt2

--- app1/models.py
+++ app1/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 from django.utils import timezone
 
 # Create your models here.
@@ -24,4 +25,4 @@ class Block(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("Block_detail", kwargs={"pk": self.pk})
+        return reverse_lazy("Block_detail", kwargs={"pk": self.pk})
