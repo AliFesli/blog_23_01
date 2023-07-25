9b537fbc8cbe4f94b129ea6082915780b2942431
ali.fesli@teilnehmer-bfwsb.de <ali.fesli@teilnehmer-bfwsb.de>
2023-07-25 15:49:02 +0200

    Model Block angelegt3

--- app1/models.py
+++ app1/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.utils import timezone
-
 # Create your models here.
 
 class Block(models.Model):
@@ -17,12 +16,13 @@ class Block(models.Model):
     created = models.DateTimeField("Erstellt", auto_now=False, auto_now_add=True)
     updated = models.DateTimeField("Geändert", auto_now=True, auto_now_add=False)
     status = models.CharField("Status", max_length=2, choices=Status.choices, default=Status.DRAFT)
+
     class Meta:
         verbose_name = "Block"
         verbose_name_plural = "Blöcke"
 
     def __str__(self):
-        return self.title
+        return f"{self.title}/{self.author} ({self.created})"
 
     def get_absolute_url(self):
         return reverse_lazy("Block_detail", kwargs={"pk": self.pk})
